793c2531bc2bef16d50a4468a55a914968894e59
Dantul <doblepiha@gmail.com>
2026-07-20 18:35:59 +0300

    Fix server port for Render

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,7 @@
-import app from './app.js';
+import app from "./app.js";
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`🚀 Server started on port ${PORT}`);
-})
+});
